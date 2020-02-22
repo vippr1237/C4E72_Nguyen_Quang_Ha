fdd13f4bd88ac4b101d09dc8ac97703dfdc7ee86
vippr1237 <vippr1239@gmail.com>
2020-02-22 23:08:23 +0700

    shdafbhjshdf

--- Homework/session4_homework/app.js
+++ Homework/session4_homework/app.js
@@ -1185,5 +1185,101 @@
 //     rt(90);
 // }
 // }
-//
+//10
+// clear()
+// setshape('rocket');
+// const cmds = [
+//     {
+//         shape: 'square',
+//         x: 20,
+//         y: 40,
+//         width: 50,
+//     },
+//     {
+//         shape: 'rect',
+//         x: 8,
+//         y: 70,
+//         width: 12,
+//         height: 40,
+//     },
+//     {
+//         shape: 'rect',
+//         x: 70,
+//         y: 70,
+//         width: 12,
+//         height: 40,
+//     },
+//     {
+//         shape: 'rect',
+//         x: 10,
+//         y: 20,
+//         width: 70,
+//         height: 20,
+//     },
+//     {
+//         shape: 'square',
+//         x: 25,
+//         y: 40,
+//         width: 14,
+//     },
+//     {
+//         shape: 'square',
+//         x: 33,
+//         y: 42,
+//         width: 6,
+//     },
+//     {
+//         shape: 'square',
+//         x: 50,
+//         y: 40,
+//         width: 14,
+//     },
+//     {
+//         shape: 'square',
+//         x: 58,
+//         y: 42,
+//         width: 6,
+//     },
+//     {
+//         shape: 'square',
+//         x: 40,
+//         y: 25,
+//         width: 8,
+//     },
+//     {
+//         shape: 'square',
+//         penWidth: 1,
+//         x: 500,
+//         y: 500,
+//         width: 15,
+//     },
+// ]
+// for (let i=0;i<cmds.length;i++){
+// home()
+// penup();
+// rt(90);
+// fd(cmds[i].x);
+// lt(90);
+// fd(cmds[i].y);
+// pendown();
+// penwidth(cmds.penwidth);
+// if (cmds[i].shape=="rect"){
+// for (let j=0;j<4;j++){
+//   if (j%2!=0){
+//      fd(cmds[i].width);
+//      rt(90);
+// }
+// else{
+//  fd(cmds[i].height);
+//  rt(90);
+// }
+// }
+// }
+// else if(cmds[i].shape=="square"){
+//   for (let j=0;j<4;j++){
+//      fd(cmds[i].width);
+//      rt(90);
+//   }
+// }
+// }
 
